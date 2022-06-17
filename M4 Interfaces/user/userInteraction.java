587b8be061736998385724a92c7d12d1e5ce6e93
SimonX13 <66461376+SimonX13@users.noreply.github.com>
2022-06-16 21:40:49 -0700

    Update userInteraction.java

--- M4 Interfaces/user/userInteraction.java
+++ M4 Interfaces/user/userInteraction.java
@@ -1,39 +1,55 @@
 
 interface userInteraction {
+
     // parameter: A string of keyword input by user
     // purpose: To allow users to search a specific facility by names
-    // return: if the facility exists, return the facility JSON file
+    // return: if facilities conatining the keyword exists, return the facilities
+    // JSON file
     // else return null
     public string searchFacilityByName(String keyword);
 
-    // parameter: integer of the id of the post, text is the user input
-    // purpose: To allow users to repsond to a post
-    // return: nothing
-    //
-    public void commentOnPost(int postID, String text);
+    // parameter: integer of the id of the facility, text is the user input,
+    // facilityType is the bottom catogry bar which user can select types on the
+    // main page
+    // purpose: Users defined by userID can repsond comments in string to a facility
+    // defined by facilityID.
+    // return: if success, return: a string indicates that users have successfully
+    // commented
+    // else return a warning message
 
-    // parameter: integer of the id of the facility, text is the user input
-    // purpose: To allow users to repsond to a facility
-    // return: nothing
-    //
-    public void commentOnFacility(int facilityID, String text);
+    public void commentOnFacility(String facilityID, String text, String userID, int facilityType);
 
     // parameter: facilityID is an integer of the id of the comment
-    // purpose: Like a comment in facility or post page
-    // return: nothing
-    //
-    public void upVote(int commentID);
+    // purpose: Users defined by senderID can like a comment which is associated
+    // with a receiverID in the facility page or the post page which are defined by
+    // facilityID
+    // return: if success, return: a string indicates that users have successfully
+    // up voted the comment
+    // else return a warning message
+
+    public void upVote(String senderID, String facilityID, String receiverID);
 
     // parameter: integer of the id of the comment
-    // purpose: Dislike a comment in facility or post page
-    // return: nothing
-    //
-    public void downVote(int commentID);
+    // purpose: Users defined by senderID can dislike a comment which is associated
+    // with a receiverID in the facility page or the post page which are defined by
+    // facilityID
+    // return: if success, return: a string indicates that users have successfully
+    // down voted the comment
+    // else return a warning message
+    public void downVote(String senderID, String facilityID, String receiverID);
 
-    // parameter: integer of the id of the facility
-    // purpose: To allow users to rate the facility from star 1 to 5
-    // return: nothing
-    //
-    public void rateTheFacility(int facilityID, int star);
+    // parameter:
+    // purpose: To allow users who are defined by userID to rate the facility which
+    // is defined by facilityID from star 1 to 5
+    // return: if success, return: a string indicates that users have successfully
+    // rated
+    // else return a warning message
+    public void rateTheFacility(String facilityID, int star, String userID);
+
+    // purpose: User can select a type of facility on the main page and the most
+    // recent added facilities would be listed first and so forth
+    // return: if success, return a JSON file with all the facilites
+    // else return a warning message
+    public void getMostRecentFacility(String facilityType);
 
 }
