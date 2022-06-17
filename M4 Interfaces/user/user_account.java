19537b7a69294e1e0bbac7401284cc79130377bc
HizanLi <80601415+HizanLi@users.noreply.github.com>
2022-06-16 21:40:21 -0700

    Add files via upload

--- M4 Interfaces/user/user_account.java
+++ M4 Interfaces/user/user_account.java
@@ -11,19 +11,23 @@ interface user_account {
     // "account_status" : 0
     // }
     
-    // parameter: String user_id, String password
+    // parameter: String user_id,  the user id to be searched in database
+    //            String password, the password to be searched in database
     // purpose:   Login in to the app with user_id and password
     // return:    if user_id and password matched in user_db, return true
     //            else return false
     public void verify_user_normal(String user_id, String password);
 
-    // parameter: String gmail
+    // parameter: String gmail, the email address to be search in databases
     // purpose:   Login in to the app with google authorization 
     // return:    if google authorization matched in user_db, return true
     //            else return false
     public void verify_user_google(String gmail);
 
-    // parameter: String id, String login_name, String password, String email
+    // parameter: String id, the user id to be stored in database
+    //            String login_name, the login_name to be stored in database
+    //            String password, the password to be stored in database
+    //            String email, the email to be stored in database
     // purpose:   create a account by sending information to database
     // return:    if user id exist in db, return "user id exist"
     //            if login name exist in db, return "login name exist"
@@ -47,73 +51,97 @@ interface user_account {
     //     "number_of_facility": 0
     //   } 
 
-    // parameter: String user_id, String username
+    // parameter: String user_id, the user_id to be search in database and update with new info
+    //            String username, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update username stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
-    public void update_user_profile_username(String user_id, String username);
+    public void update_user_profile_username(String user_id, String usernamem, int type);
 
-    // parameter: String user_id, String userLogo_link
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String userLogo_link, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_userLogo(String user_id, String userLogo_link);  
 
-    // parameter: String user_id, int number_of_credit
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String number_of_credit, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_credit(String user_id, int number_of_credit);  
 
-    // parameter: String user_id, String phone_number
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String phone_number, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_phone_number(String user_id, String phone_number);  
     
-    // parameter: String user_id, String student_id
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String student_id, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_student_id(String user_id, String student_id);  
 
-    // parameter: String user_id, String program
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String program, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_program(String user_id, String program);  
 
-    // parameter: String user_id, String gender
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String gender, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_gender(String user_id, String gender);  
 
-    // parameter: String user_id, String Biography
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String Biography, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_Biography(String user_id, String Biography);  
 
-    // parameter: String user_id, String number_of_rate
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String number_of_rate, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_number_of_rate(String user_id, String number_of_rate);  
 
-    // parameter: String user_id, String number_of_comment
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String number_of_comment, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_number_of_comment(String user_id, String number_of_comment);  
 
-    // parameter: String user_id, String number_of_reply
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String number_of_reply, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
     public void update_user_profile_number_of_reply(String user_id, String number_of_reply);  
 
-    // parameter: String user_id, String number_of_facility
+    // parameter: String user_id, the user_id to be search in database and update with new information
+    //            String number_of_facility, new information to be store in database
+    //            Int type, the number repersentation wich filed in user profile to be update
     // purpose:   update user infomation stored in database
     // return:    if successfuly change data stored in database, return true
     //            else return false
--- M4 Interfaces/user/user_report.java
+++ M4 Interfaces/user/user_report.java
@@ -8,19 +8,26 @@ interface user_report {
     //     "reason" : "why reporting"
     //   }
       
-    // parameter: String reporter_user_id, String reproted_user_id, String facility_id, String reason_to_report 
+    // parameter: String reporter_user_id, the user id of user who is reporting
+    //            String reproted_user_id, the user id of user who is being report
+    //            String reason_to_report, why user reports another user
     // purpose:   to report a user that has inappropriate behaviour 
     // return:    if report is successfuly recieved by server, return true
     //            else report is not successfuly recieved by server, return fale
-    public void report_user(String reporter_user_id, String reproted_user_id, String facility_id, String reason_to_report);
+    public void report_user(String reporter_user_id, String reproted_user_id, String reason_to_report);
 
-    // parameter: String reporter_user_id, String reproted_user_id, String facility_id, String reason_to_report 
+    // parameter: String reporter_user_id, the user id of user who is reporting
+    //            String reproted_user_id, the user id of user who is being report
+    //            String facility_id, the facility that has the reported comment
+    //            String reason_to_report, why user reports a comment    
     // purpose:   to report a user that has inappropriate behaviour 
     // return:    if report is successfuly recieved by server, return true
     //            else report is not successfuly recieved by server, return fale
     public void report_comment(String reporter_user_id, String reproted_user_id, String facility_id, String reason_to_report);
 
-    // parameter: String reporter_user_id, String reproted_user_id, String facility_id, String reason_to_report 
+    // parameter: String reporter_user_id, the user id of user who is reporting 
+    //            String facility_id, the facility is being report
+    //            String reason_to_report, why user reports a facility   
     // purpose:   to report a user that has inappropriate behaviour 
     // return:    if report is successfuly recieved by server, return true
     //            else report is not successfuly recieved by server, return fale
