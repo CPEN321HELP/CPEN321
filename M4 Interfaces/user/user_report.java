128487ad6dd4ed61beb3f557fc074110bb1ce94a
HizanLi <80601415+HizanLi@users.noreply.github.com>
2022-06-17 21:44:10 -0700

    Update user_report.java

--- M4 Interfaces/user/user_report.java
+++ M4 Interfaces/user/user_report.java
@@ -11,7 +11,7 @@ interface user_report {
     // parameter: String reporter_user_id, the user id of user who is reporting
     //            String reproted_user_id, the user id of user who is being report
     //            String reason_to_report, why user reports another user
-    // purpose:   to report a user that has inappropriate behaviour 
+    // purpose:   to report a user that has inappropriate behaviour, user will get real time notification when bein report. 
     // return:    if report is successfuly recieved by server, return true
     //            else report is not successfuly recieved by server, return fale
     public void report_user(String reporter_user_id, String reproted_user_id, String reason_to_report);
@@ -20,7 +20,7 @@ interface user_report {
     //            String reproted_user_id, the user id of user who is being report
     //            String facility_id, the facility that has the reported comment
     //            String reason_to_report, why user reports a comment    
-    // purpose:   to report a user that has inappropriate behaviour 
+    // purpose:   to report a user that has inappropriate behaviour, user will get real time notification when bein report. 
     // return:    if report is successfuly recieved by server, return true
     //            else report is not successfuly recieved by server, return fale
     public void report_comment(String reporter_user_id, String reproted_user_id, String facility_id, String reason_to_report);
@@ -28,7 +28,7 @@ interface user_report {
     // parameter: String reporter_user_id, the user id of user who is reporting 
     //            String facility_id, the facility is being report
     //            String reason_to_report, why user reports a facility   
-    // purpose:   to report a user that has inappropriate behaviour 
+    // purpose:   to report a facility that has inappropriate content, if the facility is  
     // return:    if report is successfuly recieved by server, return true
     //            else report is not successfuly recieved by server, return fale
     public void report_facility(String reporter_user_id, String facility_id, String reason_to_report);
