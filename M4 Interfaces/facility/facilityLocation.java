e6fdf88b931d268683fd55d91d0f790b8400699e
HizanLi <80601415+HizanLi@users.noreply.github.com>
2022-06-18 15:22:17 -0700

    Update facilityLocation.java

--- M4 Interfaces/facility/facilityLocation.java
+++ M4 Interfaces/facility/facilityLocation.java
@@ -1,23 +1,45 @@
 interface FacilityLocation {
+//         Example Json for facilities
+//         {
+//           "facility_id": "yyyymmdd + type_of_ facility + order",
+//           "facility_type": 0,
+//           "facility_title": "This is a title",
+//           "facility_content": "This is the content of this facility",
+//           "facility_image_link": "https://xxx.xxx.xxx",
+//           "facility_overall_rate": 0,
+//           "latitude" : 123
+//           "Longitude" : 123
+//           "rates": {
+//             "user_id1": 0,
+//             "user_id2": 0,
+//             "etc": 0
+//           },
+//           "reviews": {
+//             "user_id1": {
+//               "replier_id": "content",
+//               "number_of_upvote": "content",
+//               "number_of_downvote": "content",
+//               "reply_content": "content"
+//             },
+//             "user_id2": {
+//               "replier_id": "content",
+//               "number_of_upvote": "content",
+//               "number_of_downvote": "content",
+//               "reply_content": "content"
+//             }
+//           },
+//         }
     /*  purpose: facility latitude getter method
+        parameter: facilityProfile is a JSON file that contains the information with the  Example Json for facilities  
         return: the latitude of the associated facility 
     */
-    public double getLatitude();
+    public double getLatitude(String facilityProfile);
 
     /*  purpose: facility longitude getter method
+        parameter: facilityProfile is a JSON file that contains the information with the  Example Json for facilities  
         return: the longitude of the associated facility 
     */
-    public double getLongitude();
-
-    /*  parameter: latitude: the new latitude of the associated facility
-        purpose: facility latitude setter method
-    */
-    public void setLatitude(double latitude);
-
-    /*  parameter: longitude: the new longitude of the associated facility
-        purose: facility longitude setter method
-    */
-    public void setLongitude(double longitude);
+    public double getLongitude(String facilityProfile);
 
     /*  parameter:  latitude: latitude of the facility
                     longitude: longitude of the facility
