4188240398fa94644e2a190337a850bd3453e406
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-06-25 15:13:29 -0700

    This is the backend server with add facility
    First commit for backend with add_ facility mostly filled and  creates new branch

---
+++ backend/server.js
@@ -0,0 +1,164 @@
+var express = require('express'); 
+var app = express(); 
+  
+const bodyParser = require('body-parser'); 
+var server = app.listen(3000); 
+  
+app.use(bodyParser.json()); 
+app.use(bodyParser.urlencoded({ extended: true })); 
+  
+app.post('/postdata', (req, res) => { 
+	console.log(req.body); 
+   var data = req.body.data; // your data 
+	console.log(data); 
+
+   var user_id = req.body.user_id;
+   console.log(user_id); 
+
+   var password = req.body.password;
+   console.log(password); 
+
+   // do something with that data (write to a DB, for instance) 
+	res.status(200).json({ 
+		message: "JSON Data received successfully" 
+	}); 
+}); 
+
+//create account with email and possward
+app.post('/sign_up', (req, res) => { 
+   var user_id = generate_new_user_id(); // write a method to generate a user id for new user
+   var user_email = req.body.email;
+   var user_password = req.body.password;
+}); 
+
+//login to account with email and possward
+app.get('/sign_in', (req, res) => { 
+   var user_email = req.body.email;
+   var user_password = req.body.password;
+}); 
+
+//create account with google
+app.post('/google_sign_up', (req, res) => { 
+   var user_id = generate_new_user_id(); // write a method to generate a user id for new user
+   var user_gmail = req.body.email;
+}); 
+
+//sign in account with google
+app.get('/google_sign_in', (req, res) => { 
+   var user_gmail = req.body.email;
+}); 
+
+app.get('/post/newest', (req, res) => { 
+
+}); 
+
+app.get('/post/newest/search', (req, res) => { 
+
+}); 
+
+app.get('/entertainment/newest', (req, res) => { 
+
+}); 
+
+app.get('/entertainment/newest/search', (req, res) => { 
+
+}); 
+
+app.get('/restaurant/newest', (req, res) => { 
+
+}); 
+
+app.get('/restaurant/newest/search', (req, res) => { 
+
+}); 
+
+app.get('/study/newest', (req, res) => { 
+
+}); 
+
+app.get('/study/newest/search', (req, res) => { 
+    
+
+}); 
+
+//recieve infomation from user side
+app.post('/add_facility', (req, res) => { 
+    var newFacilityName = req.body.newFacilityName;
+    var newFacilityType = req.body.newFacilityType;
+    var newFacilityTitle = req.body.newFacilityTitle;
+    var newFacilityConetnt = req.body.newFacilityConetnt;
+    var newFacilityImage = req.body.newFacilityImage;
+    var newFacilityOverallRate = req.body.newFacilityOverallRate;
+
+    var approveByAdmin = req.body.approveByAdmin; // how should approve by admin be achieved exactly
+
+    if(approveByAdmin == true)
+    {
+        res.send(
+          {
+          "facility_id": newFacilityName,
+          "facility_type": newFacilityType,
+          "facility_title": newFacilityTitle,
+          "facility_content": newFacilityConetnt,
+          "facility_image_link": newFacilityImage,
+          "facility_overall_rate": newFacilityOverallRate,
+          //parts below need to be refined later, this is just an exaple
+          "rates": {
+            "user_id1": 0,
+            "user_id2": 0,
+            "etc": 0
+          },
+          "reviews": {
+            "user_id1": {
+              "replier_id": "content",
+              "number_of_upvote": "content",
+              "number_of_downvote": "content",
+              "reply_content": "content"
+            },
+            "user_id2": {
+              "replier_id": "content",
+              "number_of_upvote": "content",
+              "number_of_downvote": "content",
+              "reply_content": "content"
+            }
+          },
+        }
+        )
+    } else{
+        res.send("Add of facility is unsuccessful, please make sure the place actual exists and is new to our system.");
+    }
+}); 
+
+app.post('/report/facilty', (req, res) => { 
+
+}); 
+
+app.post('/remove/facility', (req, res) => { 
+
+}); 
+
+app.post('/report/comment', (req, res) => { 
+
+}); 
+
+app.post('/remove/comment', (req, res) => { 
+
+}); 
+
+app.post('/report/user', (req, res) => { 
+
+}); 
+
+app.post('/remove/user', (req, res) => { 
+
+}); 
+
+app.post('/credit/add', (req, res) => { 
+
+}); 
+
+app.post('/credit/remove', (req, res) => { 
+
+}); 
+
+
