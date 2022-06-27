eebb274c3fed7a3c854729556706d429b2497117
SimonX13 <66461376+SimonX13@users.noreply.github.com>
2022-06-26 21:04:23 -0700

    Update 321SEVER_SIMON.js

--- backend/321SEVER_SIMON.js
+++ backend/321SEVER_SIMON.js
@@ -4,26 +4,32 @@ var app = express();
 const bodyParser = require('body-parser'); 
 const { application } = require('express');
 var server = app.listen(3000); 
+
+const {MongoClient} = require("mongodb");
+const { response } = require("express");
+const uri ="mongodb://localhost:27017"
+const client = new MongoClient(uri)
+
   
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
   
-app.post('/postdata', (req, res) => { 
-	console.log(req.body); 
-   var data = req.body.data; // your data 
-	console.log(data); 
+// app.post('/postdata', (req, res) => { 
+// 	console.log(req.body); 
+//    var data = req.body.data; // your data 
+// 	console.log(data); 
 
-   var user_id = req.body.user_id;
-   console.log(user_id); 
+//    var user_id = req.body.user_id;
+//    console.log(user_id); 
 
-   var password = req.body.password;
-   console.log(password); 
+//    var password = req.body.password;
+//    console.log(password); 
 
-   // do something with that data (write to a DB, for instance) 
-	res.status(200).json({ 
-		message: "JSON Data received successfully" 
-	}); 
-}); 
+//    // do something with that data (write to a DB, for instance) 
+// 	res.status(200).json({ 
+// 		message: "JSON Data received successfully" 
+// 	}); 
+// }); 
 
 //create account with email and possward
 app.post('/sign_up', (req, res) => { 
@@ -61,29 +67,81 @@ app.get('/facilityType', (req,res)=>{
    res.status(200).send(facilityType);
 });
 
-app.get('/post/newest', (req, res) => { 
-   var newestPostId = req.body.id;
-   var newestPostTitle = req.body.title;
-   var newestfacilityType = req.body.facilityType;
-   var newestPostDescription = req.body.description;
-   var newestPostRating = req.body.rating;
+// get a detailed post -->
+app.post('/post/id', (req, res)=>{
+    res.send(req.body.text);
+});
 
-   var review = req.body.review;
- 
-   try{
-      res.status(200).send({
-         "facility_id": newestPostId,
-         "facility_type": newestfacilityType,
-         "facility_title": newestPostTitle,
-         "facility_content": newestPostDescription,
-         "facility_overall_rate": newestPostRating,
-         //parts below need to be refined later, this is just an exaple
-         "review": review,
+
+// app.post('/jj', async(req,res)=>{
+//     try{
+//         await client.db("facility").collection("posts").insertOne(req.body);
+//         res.status(200).send("TODO item added successfully");
+//     }
+//     catch(err){
+//         console.log(err);
+       
+//     }
+// });
+
+// get a list of general posts --> 
+app.get('/post/newest', async(req, res) => { 
+    
+   // use date to find 5 newest
+    try{
+        // const greatestNumber = await client.db("facility").collection("posts").find({_id : '1'}) //sort({"_id": -1}).limit(1);
+        // console.log(greatestNumber);
+        // res.send(greatestNumber);
+     
+        const result = await client.db("facility").collection("posts").find({}).sort({_id: -1}).limit(1).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.send(result);
+          });// result is the current number --- an int
+        //console.log(result);
         
-      });
-   }catch(err){
-      console.log(err);
-   }
+       // console.log("cc is"+ cc);
+// "greatestNumber"       :   greatestNumber in int
+// greatestNumber in int  :   _id
+
+
+        //await result.forEach(console.dir);
+        // const arr = []; // is the array
+        // for(let i=0; i<5; i++){
+        //   var ww = await client.db("facility").collection("posts").find(greatestNumber - i);
+        //   arr.push(ww);
+        // }
+        // res.status(200).send({
+
+        // });
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send(err);
+    }    
+//    var newestPostId = req.body.id;
+
+//    var newestPostTitle = req.body.title;
+//    var newestfacilityType = req.body.facilityType;
+//    var newestPostDescription = req.body.description;
+//    var newestPostRating = req.body.rating;
+
+//    var review = req.body.review;
+ 
+//    try{
+//       res.status(200).send({
+//          "facility_id": newestPostId,
+//          "facility_type": newestfacilityType,
+//          "facility_title": newestPostTitle,
+//          "facility_content": newestPostDescription,
+//          "facility_overall_rate": newestPostRating,
+//          //parts below need to be refined later, this is just an exaple
+//          "review": review,
+        
+//       });
+//    }catch(err){
+//       console.log(err);
+//    }
    
    //JSON object for review 
    // {
@@ -108,6 +166,8 @@ app.get('/post/newest', (req, res) => {
    
 }); 
 
+
+// return max 5 posts which title matched
 app.get('/post/newest/search', (req, res) => { 
     var keyWordSearched = req.body.input;
     // query data base
@@ -288,11 +348,12 @@ app.post('/report/facilty', (req, res) => {
  * Pre:  Place gets reported
  * Post: Place will be removed from the database
  */
-app.post('/remove/facility', removeFacility); 
+
 const removeFacility = function(req, res) {
     //remove facility 
     //how eaxtly shoud we achieve this, needs to connect database then remove
   };
+  app.post('/remove/facility', removeFacility); 
 
   //the following two reports and remove are similiar to /report/facility and /remove/facility
 app.post('/report/comment', (req, res) => { 
@@ -375,20 +436,56 @@ app.post('/credit/remove', (req, res) => {
 //running the actual server at port xxxx
 async function run(){
    try{ 
-       // await client.connect()
-       // console.log(" successfully connect to db")
-       
+       await client.connect();
+       console.log(" successfully connect to db");
        var server = app.listen(8000, (req, res) =>{
-           var host = server.address().address
-           var port = server.address().port
-           console.log("Example app is running");
+           var host = server.address().address;
+           var port = server.address().port;
+           console.log("Example app is running" + "with address" + host + "port: " + port);
        })
-
    }catch(err){
        console.log("error")
-       //await client.close()
+       await client.close()
    }
 }
+run();
 
-
-run()
+// {
+//     // (yyyymmddhhmm + type_of_facilicity+number(第几个帖子) )
+//     [
+//         "Facility_id" : 
+//         {
+//             "facility_type": 0,
+//             "facility_title": "This is a title",
+//             "facility_content": "This is the content of this facility",
+//             "facility_image_link": "https://xxx.xxx.xxx",
+//             "facility_overall_rate": 0,
+//             "numberOfRates" : 1,
+//             "rated_user": {
+//                 "user_id1": 0,
+//                 "user_id2": 0,
+//                 "etc": 0
+//             },
+//             "longtidue" : 0.01,
+//             "latitude" : 0.01,
+//             "reviews": 
+//             [
+//                     {
+//                         "replier_id": "content",
+//                         "number_of_upvote": 0,
+//                         "number_of_downvote": 00,
+//                         "reply_content": "content",
+//                         "timeOfReply" : "yyyymmddhhmmss"
+//                     },
+//                     {
+//                         "replier_id": "content",
+//                         "number_of_upvote": 00,
+//                         "number_of_downvote": 01,
+//                         "reply_content": "content",
+//                         "timeOfReply" : "yyyymmddhhmmss"
+//                     }
+//             ]
+//         }
+//     ]
+// }
+    
