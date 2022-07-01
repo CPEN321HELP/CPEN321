79bf2cc8b7e25995770a95ce927dccd0bc0fd641
SimonX13 <66461376+SimonX13@users.noreply.github.com>
2022-07-01 15:34:28 -0700

    Update 321SEVER_SIMON.js

--- backend/321SEVER_SIMON.js
+++ backend/321SEVER_SIMON.js
@@ -7,6 +7,7 @@ var server = app.listen(3000);
 
 const {MongoClient} = require("mongodb");
 const { response } = require("express");
+const e = require('express');
 const uri ="mongodb://localhost:27017"
 const client = new MongoClient(uri)
 
@@ -14,6 +15,12 @@ const client = new MongoClient(uri)
 app.use(bodyParser.json()); 
 app.use(bodyParser.urlencoded({ extended: true })); 
   
+
+app.get('/', function (req, res) {  
+    res.send("adjioubihe");
+}) 
+
+
 // app.post('/postdata', (req, res) => { 
 // 	console.log(req.body); 
 //    var data = req.body.data; // your data 
@@ -31,32 +38,38 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // 	}); 
 // }); 
 
-//create account with email and possward
-app.post('/sign_up', (req, res) => { 
-   var user_id = generate_new_user_id(); // write a method to generate a user id for new user
-   var user_email = req.body.email;
-   var user_password = req.body.password;
-}); 
-
-//login to account with email and possward
-app.get('/sign_in', (req, res) => { 
-   var user_email = req.body.email;
-   var user_password = req.body.password;
-}); 
 
 //create account with google
 app.post('/google_sign_up', (req, res) => { 
-   var user_id = generate_new_user_id(); // write a method to generate a user id for new user
-   var user_gmail = req.body.email;
+   //var user_id = generate_new_user_id(); // write a method to generate a user id for new user
+    var user_gmail = req.body.email;
+    var userName= req.body.userName;
+    client.db("user").collection("users").insertOne({
+            "email" : user_gmail, 
+            "account_type" : 0,  
+            "account_status" : 0, 
+            "username": userName, 
+            "user_logo": "https://xxx.xxx.xxx", 
+            "number_of_credit": 0, 
+            "number_of_rate": 0, 
+            "number_of_reply": 0, 
+            "number_of_facility": 0 
+            
+    })
+    res.send("added");
+   // store userJSON
 }); 
 
 //sign in account with google
 app.get('/google_sign_in', (req, res) => { 
-   var user_gmail = req.body.email;
+   var userGmail = req.body.email;
+   // send back a userJson
+   const result = client.db("user").collection("users").findOne({"email" : userGmail});
+   res.send(result);
 }); 
 
 
-//-------------------------------------------------------------------------------------
+
 //-------------------------------------------------------------------------------------
 
    //JSON object for review 
@@ -89,25 +102,11 @@ app.get('/google_sign_in', (req, res) => {
 //    res.status(200).send(facilityType);
 // });
 
-// get a detailed post -->
-app.get('/posts/id', async(req, res)=>{
-    try{
-        await client.db("facility").collection("posts").find({_id: req.body.id}).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.send(result);
-        });
-    }
-    catch(err){
-        console.log(err);
-        res.status(400).send(err);
-    }  
-});
-
+// { id : "5", facilityOverallRate: 5.0 , facilityTitle : "IKB", facilityDescription: "everyone goes there", timeAdded: "2022/06/27"}
 
 // app.post('/jj', async(req,res)=>{
 //     try{
-//         await client.db("facility").collection("posts").insertOne(req.body);
+//         await client.db("facility").collection("entertainment").insertOne(req.body);
 //         res.status(200).send("TODO item added successfully");
 //     }
 //     catch(err){
@@ -124,25 +123,48 @@ app.get('/posts/id', async(req, res)=>{
 //     }
 // });
 
+
+/**
+ * purpose: get a specific detailed look of a post
+ */
+app.post('/posts/id', async(req, res)=>{
+    try{
+        await client.db("facility").collection("posts").findOne({_id: req.body.id}).toArray(function(err, result) {
+            if (err) throw err;
+            console.log(result);
+            res.status(200).send(result);
+        });
+    }
+    catch(err){
+        console.log(err);
+        res.status(400).send(err);
+    }  
+});
+
+
 /**
  * purpose: get 5 most recent added posts 
  * explanation for teammate: sort(-1) means descending order and limits means the number of records
+ * return: an array consists of id, date, description, title, and the length of the array
  */
-// return id, date, description, title
+
 app.get('/posts/newest', async(req, res) => { 
     try{
+        const pageNumber = req.body.pageNumber;
         const bigArr = []
         await client.db("facility").collection("posts").find({}).sort({_id: -1}).limit(5).toArray(function(err, result) {
             if (err) throw err;
             try{
                 console.log(result[0]._id)
-                for(var i = 0; i < 5; i++){
+                var len = result.length;
+                for(var i = (pageNumber-1)*5; i < len; i=i+pageNumber){
                     var arr = [];
                     arr.push(result[i]._id)
                     arr.push(result[i].facility.facilityTitle)
                     arr.push(result[i].facility.facilityDescription)
                     arr.push(result[i].facility.timeAdded)
                     bigArr.push(arr);
+                    bigArr.push(arr.length);
                 }
                 res.send(bigArr)
             }catch(err){
@@ -158,8 +180,8 @@ app.get('/posts/newest', async(req, res) => {
 }); 
 
 
-// return max 5 posts which title matched
-app.get('/posts/search', async(req, res) => { 
+// return all the posts which title matched
+app.post('/posts/search', async(req, res) => { 
     var keyWordSearched = req.body.input;
     try{
         await client.db("facility").collection("posts").find({"facility.facilityTitle": keyWordSearched}).toArray(function(err, result) {
@@ -174,13 +196,15 @@ app.get('/posts/search', async(req, res) => {
     }  
 }); 
 
-app.get('/entertainment/newest', async(req, res) => { 
+app.post('/entertainment/newest', async(req, res) => { 
     try{
+        const pageNumber = req.body.pageNumber;
         const bigArr = [];
         await client.db("facility").collection("entertainment").find({}).sort({_id: -1}).limit(5).toArray(function(err, result) {
             if (err) throw err;
             try{
-                for(var i = 0; i < 5; i++){
+                var len = result.length;
+                for(var i = (pageNumber-1)*5; i < len; i=i+pageNumber){
                     var arr = [];
                     arr.push(result[i]._id)
                     arr.push(result[i].facility.facilityTitle)
@@ -202,7 +226,7 @@ app.get('/entertainment/newest', async(req, res) => {
     } 
 }); 
 
-app.get('/entertainment/search', (req, res) => { 
+app.post('/entertainment/search', async(req, res) => { 
     var keyWordSearched = req.body.input;
     try{
         await client.db("facility").collection("entertainment").find({"facility.facilityTitle": keyWordSearched}).toArray(function(err, result) {
@@ -217,7 +241,7 @@ app.get('/entertainment/search', (req, res) => {
     }  
 
 }); 
-app.get('/entertainment/id', async(req, res) => { 
+app.post('/entertainment/id', async(req, res) => { 
     try{
         await client.db("facility").collection("entertainment").find({_id: req.body.id}).toArray(function(err, result) {
             if (err) throw err;
@@ -230,7 +254,7 @@ app.get('/entertainment/id', async(req, res) => {
         res.status(400).send(err);
     }  
 }); 
-app.get('/restaurant/id' , async(req, res) => { 
+app.post('/restaurant/id' , async(req, res) => { 
     try{
         await client.db("facility").collection("restaurant").find({_id: req.body.id}).toArray(function(err, result) {
             if (err) throw err;
@@ -243,13 +267,14 @@ app.get('/restaurant/id' , async(req, res) => {
         res.status(400).send(err);
     }  
 });
-app.get('/restaurant/newest', async(req, res) => { 
+app.post('/restaurant/newest', async(req, res) => { 
+    const pageNumber = req.body.pageNumber;
     try{
         const bigArr = [];
         await client.db("facility").collection("restaurant").find({}).sort({_id: -1}).limit(5).toArray(function(err, result) {
             if (err) throw err;
             try{
-                for(var i = 0; i < 5; i++){
+                for(var i = (pageNumber-1)*5; i < len; i=i+pageNumber){
                     var arr = [];
                     arr.push(result[i]._id)
                     arr.push(result[i].facility.facilityTitle)
@@ -271,7 +296,7 @@ app.get('/restaurant/newest', async(req, res) => {
     } 
 }); 
 
-app.get('/restaurant/search', (req, res) => { 
+app.post('/restaurant/search', async(req, res) => { 
     var keyWordSearched = req.body.input;
     try{
         await client.db("facility").collection("restaurant").find({"facility.facilityTitle": keyWordSearched}).toArray(function(err, result) {
@@ -286,7 +311,7 @@ app.get('/restaurant/search', (req, res) => {
     }  
 }); 
 
-app.get('/study/id', async(req, res)=>{
+app.post('/study/id', async(req, res)=>{
     try{
         await client.db("facility").collection("study").find({_id: req.body.id}).toArray(function(err, result) {
             if (err) throw err;
@@ -300,13 +325,14 @@ app.get('/study/id', async(req, res)=>{
     }  
 });
 
-app.get('/study/newest', async(req, res) => { 
+app.post('/study/newest', async(req, res) => { 
+    const pageNumber = req.body.pageNumber;
     try{
         const bigArr = [];
         await client.db("facility").collection("study").find({}).sort({_id: -1}).limit(5).toArray(function(err, result) {
             if (err) throw err;
             try{
-                for(var i = 0; i < 5; i++){
+                for(var i = (pageNumber-1)*5; i < len; i=i+pageNumber){
                     var arr = [];
                     arr.push(result[i]._id)
                     arr.push(result[i].facility.facilityTitle)
@@ -328,7 +354,7 @@ app.get('/study/newest', async(req, res) => {
     } 
 }); 
 
-app.get('/study/search', (req, res) => { 
+app.post('/study/search', async(req, res) => { 
     var keyWordSearched = req.body.input;
     try{
         await client.db("facility").collection("study").find({"facility.facilityTitle": keyWordSearched}).toArray(function(err, result) {
@@ -343,73 +369,141 @@ app.get('/study/search', (req, res) => {
     }  
 }); 
 
+
 // -------------------------------------------------------------
-app.post('/add_facility', async (req, res) => { 
-    // var newFacilityName = req.body.newFacilityName;
-    // var newFacilityType = req.body.newFacilityType;
-    // var newFacilityTitle = req.body.newFacilityTitle;
-    // var newFacilityConetnt = req.body.newFacilityConetnt;
-    // var newFacilityImage = req.body.newFacilityImage;
-    // var newFacilityOverallRate = req.body.newFacilityOverallRate;
+/**
+ *  can we store this in a temporary database
+ */
+app.post('/requestFacility/user', async (req, res) =>{
+    const title = req.body.title             
+    const description = req.body.description
+    const location = req.body.location
+    var approved = false;
+    const type = req.body.facilityType; 
 
-    var approveByAdmin = req.body.approveByAdmin; // how should approve by admin be achieved exactly
-    //this process needs to be modified later after front end sets how we should approve
+    var date_ob = new Date();
+    var year = date_ob.getFullYear;
+    var month = date_ob.getMonth;
+    var date = date_ob.getDate;
+    var hours = date_ob.getHours();
+    var minutes = date_ob.getMinutes();
+    var seconds = date_ob.getSeconds();
+    const timeAdded = year + "/" + month + "/" +  date + "/" +  hours + "/" +  minutes + "/" + seconds;
+    
+    // await client.db("facility").collection(type).insertOne(req.body); // in this case, frontend send us a JSON file with complete information
+    await client.db("facility").collection(type).insertOne({
+        "facility": 
+                {
+                    "facilityType": type,
+                    "facilityTitle":title,
+                    "facilityDescription": description,
+                    "facilityImageLink": "",
+                    "facilityOverallRate": 0,
+                    "numberOfRates" : 1,
+                    
+                    "location" : location,
+                    
+                    "timeAdded": timeAdded
+                },
+        "rated_user": {},
+        "reviews": {}
+    });
+    res.send("Requested item added successfully");
+    //await client.db("facility").collection("facilities").find(facility_title: addedFacilityTitle{$exists: true}))
+});
 
-    if(approveByAdmin == true && db("mydb").collection("facilities").find(facility_title: addedFacilityTitle{$exists: true})) // this condtion check needs to be modified later
-    {
-        // res.send(
-        //   {
-        //   "facility_id": newFacilityName,
-        //   "facility_type": newFacilityType,
-        //   "facility_title": newFacilityTitle,
-        //   "facility_content": newFacilityConetnt,
-        //   "facility_image_link": newFacilityImage,
-        //   "facility_overall_rate": newFacilityOverallRate,
-          //parts below need to be refined later, this is just an exaple
-        //   "rates": {
-        //     "user_id1": 0,
-        //     "user_id2": 0,
-        //     "etc": 0
-        //   },
-        //   "reviews": {
-        //     "user_id1": {
-        //       "replier_id": "content",
-        //       "number_of_upvote": "content",
-        //       "number_of_downvote": "content",
-        //       "reply_content": "content"
-        //     },
-        //     "user_id2": {
-        //       "replier_id": "content",
-        //       "number_of_upvote": "content",
-        //       "number_of_downvote": "content",
-        //       "reply_content": "content"
-        //     }
-        //   },
-
-        // }
-        // )
+// figure out what is sent from the frontend; i want a boolean and id? if id is not maybe which post u are dealing with
+// display to 5 items
+app.post('/RequestFacility/Admin', async (req, res) =>{
+    const pageNumber = req.body.pageNumber;
+    const bigArr = [];
+    await client.db("facility").collection("test4").find({}).sort({_id: -1}).limit(5).toArray(function(err, result) {
+        if (err) throw err;
         try{
-            const result = await client.db("facility").collection("facilities").insert
-            (
-                {facility_id: 1234, 
-                facility_type: 00, 
-                facility_title:"The title" ,
-                facility_content: "The content",
-                facility_image_link: "http:// the link",
-                }).toArray(function(err, result) {
-            if (err) throw err;
-            console.log(result);
-            res.send(result);
-              });
-        }
-        catch(err){
-            console.log(err);
+            var len = result.length;
+            for(var i = (pageNumber-1)*5; i < len; i=i+pageNumber){
+                var arr = [];
+                arr.push(result[i]._id)
+                arr.push(result[i].facility.facilityTitle)
+                arr.push(result[i].facility.facilityDescription)
+                arr.push(result[i].facility.timeAdded)
+                //arr.push(result[i].facility.facilityOverallRate)
+                bigArr.push(arr);
+            }
+            res.send(bigArr)
+        }catch(err){
+            console.log(err)
             res.status(400).send(err);
-        }    
-    } else{
-        res.send("Add of facility is unsuccessful, please make sure the place actual exists and is new to our system.");
-    }
-}); 
+        }
+    });
+});
+
+
+// use id to fetch a specific 
+app.post('/RequestFacility/Admin/Approval', async (req, res) =>{
+    const approval = req.body.approval;
+    const id = req.body._id;
+    const temp = await client.db("facility").collection("test4").find({_id : id});
+    const type = temp.facility.facilityType;
+    const name = temp.facility.facilityTitle;
+    // firstly ensure if the facility is already existed in the db 
+    client.db("facility").collection(type).find({"facility.facilityTitle":name}).toArray(function(err, result) {
+        if(result.length !== 0){
+            // oops
+            res.send("This facility already exists");
+        }
+        else{
+            if(approval == true){
+                client.db("facility").collection(type).insertOne(temp);
+                res.send(" Admin approved the request!");
+            }
+            else{
+                res.send(" Admin disapproved the request");
+            }
+        }
+    })
+});
+
+// app.post('/add_facility', async (req, res) => { 
+//     // first we need to determine the post type 
+//     // receive a approved or rejected boolean field 
+//     // and then change the approved boolean field 
+//     //determine
+
+//     var approveByAdmin = req.body.approveByAdmin; // how should approve by admin be achieved exactly 
+//     //this process needs to be modified later after front end sets how we should approve
+//     const rr = await client.db("facility").collection("facilityTemp").find({}).sort({_id: -1}).limit(1);
+//     const name = rr.facility.facilityTitle; 
+//     if(){
+       
+//     }
+
+
+
+//     if(approveByAdmin == true && db("facility").collection("facilities").find(facility_title: addedFacilityTitle{$exists: true})) // this condtion check needs to be modified later
+//     {
+//         try{
+//             await client.db("facility").collection("facilities").insert
+//             (
+//                 {facility_id: 1234, 
+//                 facility_type: 00, 
+//                 facility_title:"The title" ,
+//                 facility_content: "The content",
+//                 facility_image_link: "http:// the link",
+//                 }).toArray(function(err, result) {
+//             if (err) throw err;
+//             console.log(result);
+//             res.send(result);
+//               });
+//         }
+//         catch(err){
+//             console.log(err);
+//             res.status(400).send(err);
+//         }    
+//     } else{
+//         res.send("Add of facility is unsuccessful, please make sure the place actual exists and is new to our system.");
+//     }
+// }); 
 
 
 
