8a88b0d644210d574fa27a5926d2d35e771e12a3
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-07-01 17:55:26 -0700

    Update exampleOfProcessingreport.js

--- backend/exampleOfProcessingreport.js
+++ backend/exampleOfProcessingreport.js
@@ -1,3 +1,53 @@
+app.post('/user/Report',
+    async function (req, res) {
+        var myDb = db.db("myDB");
+      
+        var myCollection
+        var report_Type = req.body.report_Type;
+        if(report_Type == "user"){
+            myCollection = "reportedUser";
+            var reportedUserID = req.body.user_gmail;
+            var reportReasonUser = req.body.reason;
+            var accountStatus = req.body.accountStatus;
+            var userObj = {
+                report_Type: report_Type,
+                reportedUserid: reportedUserID,
+                accountStatus: accountStatus,
+                reason: reportReasonUser
+            };
+            await client.db(myDb).collection(myCollection).insertOne(userObj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted"+res);
+                // db.close();
+                res.send(userObj);
+              });
+          
+        }else if (report_Type == "facility"){
+            myCollection = "reportedFacility";
+            var reportedFacilityID = req.body.facility_id;
+            var reportedFacilityType = req.body.facility_type;
+            var reporterID = req.body.user_id;
+            var reportReason = req.body.reason;
+            var facilityObj = { 
+                          report_Type: report_Type,
+                          facility_id: reportedFacilityID,
+                          facility_type: reportedFacilityType,
+                          reporter_id: reporterID,
+                          reason: reportReason
+                          }; //replace with real facility name or id or equivalent
+            await client.db(myDb).collection(myCollection).insertOne(facilityObj, function(err, res) {
+                if (err) throw err;
+                console.log("1 document inserted"+res);
+                // db.close();
+                res.send(facilityObj);
+              });
+        }else{
+            myCollection = "reportedComment";
+            //TBD
+        }
+       
+    }
+)
 app.get('/adminApprovalFacility',
 	async function (req, res) {
         if(req.body.approve == 0){
