ab259955fe00bb6e6d4971fbca171528de4c2637
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-07-01 22:41:46 -0700

    Update exampleOfProcessingreport.js

--- backend/exampleOfProcessingreport.js
+++ backend/exampleOfProcessingreport.js
@@ -8,7 +8,8 @@ app.post('/user/Report',
     async function (req, res) {
         var myDb = db.db("myDB");
       
-        var myCollection
+        var myCollection;
+
         var report_Type = req.body.report_Type;
         if(report_Type == "user"){
             myCollection = "reportedUser";
@@ -26,8 +27,7 @@ app.post('/user/Report',
                 console.log("1 document inserted"+res);
                 // db.close();
                 res.send(userObj);
-              });
-          
+              });         
         }else if (report_Type == "facility"){
             myCollection = "reportedFacility";
             var reportedFacilityID = req.body.facility_id;
@@ -46,7 +46,48 @@ app.post('/user/Report',
                 console.log("1 document inserted"+res);
                 // db.close();
                 res.send(facilityObj);
+                var url = 'http://localhost:8081/adminApprovalFacility'; //uti needs to be replaced with cloud uri later
+                request(url, function(err, response, body) {
+                    if (err != null) {
+                        var s = util.format("Error: %s", err);
+                        console.log(s);
+                        res.send(s);
+                        return;
+                    }else{
+                      if (body != "report is unsuccessful"){
+                          var newvalues = { $set: {temp_id: body} };
+                          await client.db(myDb).collection(myCollection).updateOne(facilityObj,newvalues,function(err, res) {
+                              if (err) throw err;
+                              console.log("1 document updated" + res);
+                              // db.close();
+                              var delteFacilityID;
+                              var deleteFacilityType;
+                              await client.db(myDb).collection(myCollection).findOne({temp_id:body}, function(err, result) {
+                                  if (err) throw err;
+                                  console.log(result.facility_id+result.facility_type);
+                                  // db.close();
+                                  delteFacilityID = result.facility_id;
+                                  deleteFacilityType = result.facility_type; 
+                                  
+                                  var facilityFinalQuey = { facility_id: delteFacilityID,
+                                                            facility_type: deleteFacilityType};
+                                  var finalFacilityStatus = {$set: {facility_status: "Status Reported"} }; //need to confirm how exactly should status look like
+                                  await client.db("facility").collection("facilities").updateOne(facilityFinalQuey, finalFacilityStatus, function(err, res) {
+                                    if (err) throw err;
+                                    console.log(res);
+                                    db.close();
+                                  });
+                                }); 
+                              
+                            });
+                                         
+                      }else{
+                          res.send("Report rejected, please try again.");
+                      }
+                    }
+                  });
               });
+        
         }else{
             myCollection = "reportedComment";
             //TBD
@@ -54,44 +95,24 @@ app.post('/user/Report',
        
     }
 )
-app.get('/adminApprovalFacility',
+
+app.post('/admin/ApprovalofFacility',
 	async function (req, res) {
         if(req.body.approve == 0){
             res.send ("report is unsuccessful");
         }
         else{
             res.send(req.body.temp_id); //would it be better to just get name of faility?
-        }
-        
+        }      
         
 	}
 )
 
-app.get('/processreportedFacility',
-	async function (req, res) {
-        var url = 'http://localhost:8081/adminApprovalFacility';
-        request(url, function(err, response, body) {
-            if (err != null) {
-                var s = util.format("Error: %s", err);
-                console.log(s);
-                res.send(s);
-                return;
-            }
-            else if (err == null && body == "report is unsuccessful")
-            {
-                res.send("Nothing happens");
-            }else{
-                var myDb = db.db("myDB");
-                var myCollection = "facility"
-                var myquery = { facility: 'Facility To Be Deleted'}; //replace with real facility name or id or equivalent
-                var newvalues = { $set: {facility_status: "removed status"} }; // this ststaus needs to be confirmed later
-                await myDb.collection(myCollection).updateOne(myquery, newvalues, function(err, res) {
-                    if (err) throw err;
-                    console.log("1 document updated" + res);
-                    // db.close();
-                  });
-                
-            }
-            });          
-	}
-)
+
+var server = app.listen(
+    8081,
+    function () {
+	var host = server.address().address;
+	var port = server.address().port;
+	console.log("Example app listening at http://%s:%s" , host, port);
+    });
