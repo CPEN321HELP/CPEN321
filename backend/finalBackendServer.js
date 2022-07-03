4bd0171bf8fda3eefcbfe2c75b598acde83c38d2
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-07-02 20:08:54 -0700

    Create finalBackendServer.js

---
+++ backend/finalBackendServer.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var app = express();
+
+const bodyParser = require('body-parser');
+const { application } = require('express');
+
+var util = require('util');
+var encoder = new util.TextEncoder('utf-8');
+const { MongoClient } = require("mongodb");
+const { response } = require("express");
+const uri = "mongodb://localhost:27017"
+const client = new MongoClient(uri)
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//Part1: Create and Manage Account Use Case
+app.post('/google_sign_up', (req, res) => { 
+    //get request from frontend with these information
+    var user_gmail = req.body.email;
+    var userName= req.body.userName;
+     client.db("Help!Db").collection("User").insertOne({
+             "email" : user_gmail, 
+             "account_type" : 0,  
+             "account_status" : 0, 
+             "username": userName, 
+             "user_logo": "https://xxx.xxx.xxx", 
+             "number_of_credit": 0, 
+             "number_of_rate": 0, 
+             "number_of_reply": 0, 
+             "number_of_facility": 0 
+             
+     })
+     res.send("added user with key " + user_gmail);
+ }); 
