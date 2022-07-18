76b3c8320a198feab702c441d56a07bdf66c8e6a
HizanLi <L2542293790@gmail.com>
2022-07-18 14:00:35 -0700

    1

--- M4 Interfaces/credit/levelnotifier.java
+++
@@ -1,9 +0,0 @@
-	// parameter: String user id of the user who will get granted or deducted points     after granting or deduction takes
-    //  place
-    // purpose:   to inform the user with real time updates when they get deducted or added with credits
-    // return:    the points added/deducted if there is addition deduction in place
-    //            if addition of the new facility is unscueesful tell user "add is disapproved by admins, please
-    //            try again with valid information of new facilites."  
-    public void levelNotifier(String userID);
-
-
