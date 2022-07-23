bd36b65631c8c50a1ab48ddca9c1821d098bba33
SimonX13 <66461376+SimonX13@users.noreply.github.com>
2022-07-22 21:29:48 -0700

    Add files via upload

---
+++ M8Backend/modulization/credit/creditHandlingNormal.js
@@ -0,0 +1,25 @@
+const creditCalculation = require("./creditCalculation");
+const findTheUser = require("/home/azureuser/Test 1/user/findTheUser");
+
+async function creditHandlingNormal (client, AdditionType, goodUserId){
+    const additionCredit_addFacility = 5;
+    const additionCredit_comment = 1;
+    const result = await findTheUser(client, goodUserId)
+    
+    console.log("the user profile(handled by credit ) is ");
+    console.log(result);
+    var currentadderCredits = result.number_of_credit;
+
+    if (AdditionType === "addFacility") {
+        currentadderCredits += additionCredit_addFacility;
+        await creditCalculation(client, goodUserId, additionCredit_addFacility)  
+    } else if (AdditionType === "comment") {
+        currentadderCredits += additionCredit_comment;
+        await creditCalculation(client, goodUserId, additionCredit_comment)  
+    } else {
+        res.send("No credits granted since no contributions made, please make contribution before any credit is granted; AdditionType is not matched in this case!");
+    }
+     
+}
+
+module.exports = creditHandlingNormal;
