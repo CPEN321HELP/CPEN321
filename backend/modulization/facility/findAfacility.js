0d36bf99e2259bd056ac437dcc86dc462f1b6609
SimonX13 <simonxia13@icloud.com>
2022-07-29 22:51:35 -0700

    try2

--- backend/modulization/facility/findAfacility.js
+++
@@ -1,5 +0,0 @@
-async function findAfacility(client, type, id){
-    const result = await client.db("Help!Db").collection(type).findOne({ _id: id});
-    return result;
-}
-module.exports = findAfacility;
