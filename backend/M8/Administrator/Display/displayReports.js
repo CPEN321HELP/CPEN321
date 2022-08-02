e60f4c9812e3a41c5e1bc13739b721639f8305b5
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-08-01 20:49:36 -0700

    Update displayReports.js

--- backend/M8/Administrator/Display/displayReports.js
+++ backend/M8/Administrator/Display/displayReports.js
@@ -1,24 +1,18 @@
 async function displayReports(client){
-    // var mycollection;
-    // if(scenario == 0){
-    //     return {report_content:[], length:0, status:200}
-    // }else if(scenario == 1){
-    //     return {report_content:["oldest 1"], length:1, status:200}
-    // }else if(scenario == 2){
-    //     return {report_content:["oldest 1", "oldest 2"], length:2, status:200}
-    // }
-    await client.db("Help!Db").collection(mycollection).find({}).sort({}).limit(2).toArray(function (err, result) {
+    try{
+    const result = await client.db("Help!Db").collection("reportedComment").find({}).sort({}).limit(2).toArray()
         var resultArray = [];
         resultArray.length = 0;
-        if(result.length == 0){resultArray.length = 0;}else{
+        if(result.length === 0){resultArray.length = 0;}else{
             for(var i=0; i<2; i++){
                 resultArray[i] = result[i];
                 if( resultArray[i] = result[i]){resultArray.length += 1;}
                 
             }
         }
-        return JSON.stringify({report_content:resultArray, length:resultArray.length, status:200});
-    });
+        var rer = JSON.stringify({report_content:resultArray, length:resultArray.length, status:200})
+    }catch(err){return err}
+    return rer;
 }
 
 module.exports = displayReports;
