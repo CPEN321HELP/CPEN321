06e358cce176935d33ad0b84353327e371d897e4
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-08-01 20:56:35 -0700

    Update displayReports.js

--- backend/M8/Administrator/Display/displayReports.js
+++ backend/M8/Administrator/Display/displayReports.js
@@ -1,18 +1,18 @@
 async function displayReports(client){
     try{
-    const result = await client.db("Help!Db").collection("reportedComment").find({}).sort({}).limit(2).toArray()
+        const result = await client.db("Help!Db").collection("reportedComment").find({}).sort({}).limit(2).toArray()
         var resultArray = [];
         resultArray.length = 0;
         if(result.length === 0){resultArray.length = 0;}else{
             for(var i=0; i<2; i++){
                 resultArray[i] = result[i];
-                if( resultArray[i] = result[i]){resultArray.length += 1;}
+                if( resultArray[i] == result[i]){resultArray.length += 1;}
                 
             }
         }
-        var rer = JSON.stringify({report_content:resultArray, length:resultArray.length, status:200})
+        var displayResult = JSON.stringify({report_content:resultArray, length:resultArray.length, status:200})
     }catch(err){return err}
-    return rer;
+    return displayResult;
 }
 
 module.exports = displayReports;
