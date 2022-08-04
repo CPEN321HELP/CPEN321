40d1c438893034e345c8a60d4395e244d95638a8
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-08-03 21:26:04 -0700

    Update findAfacility.js

--- backend/M8/facility/FacilityDisplay/findAfacility.js
+++ backend/M8/facility/FacilityDisplay/findAfacility.js
@@ -3,24 +3,24 @@ const SomeClass = require("./typeSelection");
 module.exports = class FindFacility{
     async findAfacility(client, type, id , data){
         const some = new SomeClass()
+        const type2 = await some.typeSelection(type);
         if(!client && !type && !id){
             return {"result":"unsuccesful find with missing field"}
         }
         if(typeof id != "number"){
-            return {"result":"in valid id"}
+            return {"result":"Invalid id"}
         }
         if(data ==="array"){
             return await client.db("Help!Db").collection(type2).find({}).sort({ _id: -1 }).limit(1).toArray();  
         }
-        const type2 = await some.typeSelection(type);
+        if(data.includes("@gmail.com")){
+            return await client.db("Help!Db").collection(type2).findOne({_id: id, "reviews.replierID" : data}) ;
+        }
         try{
             const result = await client.db("Help!Db").collection(type2).findOne({ _id: id});
             return result;
-        }catch(err){
-            // console.log(err);
-            // return JSON.stringify({"h":err})
-        }
+        }catch(err){}
     }
 }
 
-//module.exports = findAfacility;
+// module.exports = findAfacility;
