685fe23c7cf08dd80b22d42199ef418f9e26a6d4
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-08-03 21:35:13 -0700

    Update logicOfAddFacility.js

--- backend/M8/facility/facilityManagement/logicOfAddFacility.js
+++ backend/M8/facility/facilityManagement/logicOfAddFacility.js
@@ -1,16 +1,27 @@
 function logicOfAddFacility(lastOne){
-    var newId = "1";
-    if (lastOne.length === 0) {
-    // implying that the collection is empty
-    }
+    var newId = 1;
+    console.log("lastOne")
+    console.log(lastOne)
+    // if (JSON.stringify(lastOne)  == JSON.stringify({"result":"Invalid id"})) {
+    // // implying that the collection is empty
+    //     console.log("empty db")
+    //     return newId;
+    // }
+    if (lastOne == null) {
+        // implying that the collection is empty
+            console.log("empty db")
+            return newId;
+        }
     else {
+        
         var theidOfTheLastOne = lastOne[lastOne.length - 1]._id;
-        var theidOfTheLastOneInt = parseInt(theidOfTheLastOne);
+        var theidOfTheLastOneInt = parseInt(theidOfTheLastOne,10);
         theidOfTheLastOneInt += 1;
         console.log("dd now is : " + theidOfTheLastOneInt);
         newId = theidOfTheLastOneInt;
         console.log("new id  now is : " + newId);
+        return newId;
     }
-    return newId;
+    
 }
 module.exports = logicOfAddFacility;
