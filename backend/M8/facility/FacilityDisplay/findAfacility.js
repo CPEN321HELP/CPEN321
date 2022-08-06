b2902f85af898af86e6bb9a1770a35df6428de39
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-08-05 18:53:50 -0700

    Update findAfacility.js

--- backend/M8/facility/FacilityDisplay/findAfacility.js
+++ backend/M8/facility/FacilityDisplay/findAfacility.js
@@ -3,22 +3,31 @@ const SomeClass = require("./typeSelection");
 module.exports = class FindFacility{
     async findAfacility(client, type, id , data){
         const some = new SomeClass()
-        const type2 = await some.typeSelection(type);
-        if(!client && !type && !id){
+        
+        if(!client || !id){
             return {"result":"unsuccesful find with missing field"}
         }
-        if(typeof id != "number"){
-            return {"result":"Invalid id"}
+        else if(!type && type!=0){
+            return {"result":"unsuccesful find with missing field"}
         }
+        // else if(typeof type != "number"){
+        //     return {"result" : "typewrong"}
+        // }
+        // else if(typeof id != "number"){
+        //     return {"result":"Invalid id"}
+        // }
+        const type2 = await some.typeSelection(type);
         if(data ==="array"){
             return await client.db("Help!Db").collection(type2).find({}).sort({ _id: -1 }).limit(1).toArray();  
         }
-        if(data.includes("@gmail.com")){
+        else if(data.includes("@gmail.com")){
             return await client.db("Help!Db").collection(type2).findOne({_id: id, "reviews.replierID" : data}) ;
         }
         try{
             const result = await client.db("Help!Db").collection(type2).findOne({ _id: id});
+            console.log(result)
             return result;
+            // return JSON.stringify(result);
         }catch(err){}
     }
 }
