648856aa163fd1546748ef25f284e5d3bec80f31
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-08-05 20:15:48 -0700

    Update View_Facility.test.js

--- backend/M8/intergrationTest/View_Facility.test.js
+++ backend/M8/intergrationTest/View_Facility.test.js
@@ -1,96 +1,74 @@
 const express = require("express"); // import express
 const app = express(); //an instance of an express app, a 'fake' express app
 const request = require('supertest');
-const server  = require('../server5.js');
+const server  = require('../server6.js');
 
 app.use("/", server); //routes
-describe("testing-basic-get", () => {
-  it("GET / server ip - success", async () => {
-    const { body } = await request(app).get("/"); //uses the request function that calls on express app instance
-    expect(body).toEqual({ "Result": "Connected to server at http://20.213.243.141:3000/"});
-  });
-});
+// describe("testing-basic-get", () => {
+//   it("GET / server ip - success", async () => {
+//     const { body } = await request(app).get("/"); //uses the request function that calls on express app instance
+//     // expect(body).toEqual({ "Result": "Connected to server at http://20.213.243.141:3000/"});
+//   });
+// });
 
 app.use("/specific", server); //routes
 
 //
-describe("testing specific", () => {
-  it("POST / specific type", async () => {
+describe("testing specific 1", () => {
+    it("POST / specific type", async () => {
     //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
-    const res = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
-    expect(res.body).toEqual({
-        "_id" : 1,
-        "facility" : {
-                "facility_status" : "normal",
-                "facilityType" : "studys",
-                "facilityTitle" : "The Irving K. Barber Learning Centre",
-                "facilityDescription" : "The Irving K. Barber Learning Centre is a facility at the Vancouver campus of the University of British Columbia. The Learning Centre is built around the refurbished core of the 1925 UBC Main Library.",
-                "timeAdded" : "2022/07/06",
-                "facilityImageLink" : "https://cdn.discordapp.com/attachments/984213736652935230/994316357028036738/unknown.png",
-                "facilityOverallRate" : 2.5,
-                "numberOfRates" : 1,
-                "longitude" : -123.2527,
-                "latitude" : 49.2675
-        },
-        "ratedUser" : [
-                {
-                        "replierID" : "l2542293790@gmail.com"
-                }
-        ],
-        "reviews" : [
-                {
-                        "replierID" : "l2542293790@gmail.com",
-                        "userName" : "Linxin Li",
-                        "rateScore" : 2.5,
-                        "upVotes" : 0,
-                        "downVotes" : 0,
-                        "replyContent" : "testing ",
-                        "timeOfReply" : "2022/6/18/23/41/5"
-                }
-        ],
-        "adderID" : ""
-    })    
+        const res = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
         expect(res.statusCode).toEqual(200)
     });
-
+    it("POST / specific type", async () => {
+        //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
+        const res = await request(app).post("/specific").send({"facilityType": null , "facility_id" : "1"})
+            expect(res.statusCode).toEqual(404)
+    });
+    it("POST / specific type", async () => {
+        //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
+        const res = await request(app).post("/specific").send({"facilityType": "1" , "facility_id" : null})
+        expect(res.statusCode).toEqual(404)
+    });
+    it("POST / specific type", async () => {
+        //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
+        const res = await request(app).post("/specific").send({"facilityType": "1" , "facility_id" : "200"})
+        expect(res.body).toEqual({ "result": "null" })
+    });
+    it("POST / specific type", async () => {
+        //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
+        const res = await request(app).post("/specific").send({"facilityType": "200" , "facility_id" : "1"})
+        expect(res.body).toEqual({ "result": "null" })
+    });
 });
+
 
  
 describe("testing specific that the facility has invalid facility type", () => {
   it("POST / specific type", async () => {
-    //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
-    const res = await request(app).post("/specific").send({"facilityType":"jio" , "facility_id" : "1"})
-    expect(res.body).toEqual({"result" : "null"})    
-        expect(res.statusCode).toEqual(404)
+        const res = await request(app).post("/specific").send({"facilityType":"jio" , "facility_id" : "1"}) 
+        expect(res.body).toEqual( {"result":"unsuccesful find with missing field"})
     });
 });
  
+
 describe("testing specific that the facility has invalid facility type", () => {
   it("POST / specific type", async () => {
     //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
     const res = await request(app).post("/specific").send({"facilityType":"@@@" , "facility_id" : "1"})
-    expect(res.body).toEqual({"result" : "null"})    
-        expect(res.statusCode).toEqual(404)
+        expect(res.body).toEqual( {"result":"unsuccesful find with missing field"})    
+        //expect(res.body).toEqual(404)
     });
 });
+
 describe("testing specific that facility has invalid facility id", () => {
     it("POST / specific type", async () => {
       //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
-      const res = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "@@@"})
-      expect(res.body).toEqual({"result" : "null"})    
-          expect(res.statusCode).toEqual(404)
+        const res = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "@@@"})
+        expect(res.body).toEqual( {"result":"unsuccesful find with missing field"})    
+        //expect(res.body).toEqual(404)
       });
 });
-describe("testing specific that the facility does not exist", () => {
-    it("POST / specific type", async () => {
-      //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
-      const res = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "200"})
-      expect(res.body).toEqual({"result" : "null"})    
-          expect(res.statusCode).toEqual(404)
-      });
-});
-
-
 
 
 
@@ -111,8 +89,8 @@ describe("testing find many", () => {
     it("POST / many newest facilities ", async () => {
         //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
         const res = await request(app).post("/facility/newest").send({'type':'8'})
-        expect(res.body).toEqual({"result" : "null"})    
-        expect(res.statusCode).toEqual(404)
+        expect(res.body).toEqual( {"result":"unsuccesful find with invalid type"} )    
+        expect(res.statusCode).toEqual(400)
         });
 });
 
@@ -121,17 +99,20 @@ describe("testing find many", () => {
     it("POST / many newest facilities ", async () => {
         //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
         const res = await request(app).post("/facility/newest").send({'type':'*&^%'})
-        expect(res.body).toEqual({"result" : "null"})    
-        expect(res.statusCode).toEqual(404)
+        expect(res.body).toEqual( {"result":"unsuccesful find with invalid type"})    
+        expect(res.statusCode).toEqual(400)
         });
-    });
+});
+describe("testing find many", () => {
+    it("POST / many newest facilities ", async () => {
+        //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
+        const res = await request(app).post("/facility/newest").send({'type': null})
+        expect(res.body).toEqual( {"result":"unsuccesful find with invalid type"})    
+        expect(res.statusCode).toEqual(400)
+        });
+});
 
-    
-
-
-
-
-//-----------------------------------------------------
+// //-----------------------------------------------------
 
 app.use("/facility/search", server); //routes
 
@@ -148,17 +129,18 @@ describe("testing search", () => {
 describe("testing search", () => {
     it("POST / search facilities ", async () => {
         //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
-        const res = await request(app).post("/facility/search").send({'type':'1', "search": "$$"})
-        //expect(res.body).toEqual({"result" : "null"})    
-        expect(res.statusCode).toEqual(404)
-        });
+        const res = await request(app).post("/facility/search").send({'type':'1', "search": "@@"})
+        //expect(res.body).toEqual({"result" : [], "length":0})    
+        expect(res.statusCode).toEqual(200)
     });
+});
     
 describe("testing search", () => {
     it("POST / search facilities ", async () => {
         //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
         const res = await request(app).post("/facility/search").send({'type':'1', "search": ""})
-        //expect(res.body).toEqual({"result" : "null"})    
+      
+        expect(res.body).toEqual({"result":"unsuccesful search with missing field"})    
         expect(res.statusCode).toEqual(404)
         });
     });
@@ -167,63 +149,31 @@ describe("testing search", () => {
     it("POST / search facilities ", async () => {
         //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
         const res = await request(app).post("/facility/search").send({'type':'**', "search": ""})
-        //expect(res.body).toEqual({"result" : "null"})    
+        //expect(res.body).toEqual({"result":"unsuccesful find with invalid type"})    
         expect(res.statusCode).toEqual(404)
         });
     });
 
-
+    describe("testing search", () => {
+        it("POST / search facilities ", async () => {
+            //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
+            const res = await request(app).post("/facility/search").send({'type':'10', "search": "j"})
+            //expect(res.body).toEqual({"result":"unsuccesful find with invalid type"})    
+            expect(res.statusCode).toEqual(404)
+            });
+        });
+    
 describe("testing search", () => {
     it("POST / search facilities ", async () => {
         //const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
         const res = await request(app).post("/facility/search").send({'type':'1', "search": "1234567891011123456789ijhnbfdstyukjmnbvdsetyujhgbvcsaewrtyujhnbvdfswetyujhnbvcdsewtyhgbvcx"})
-        expect(res.body).toEqual({"result" : "null"})    
-        expect(res.statusCode).toEqual(404)
-        });
+        expect(res.body).toEqual({"result" : [], "length":0})    
+       // expect(res.statusCode).toEqual(404)
     });
+});
 
 
 
 
 
-
-// app.use("/specific", server); //routes
-// describe("testing specific", () => {
-//   it("POST / specific type", async () => {
-//     const { body } = await request(app).post("/specific").send({"facilityType":"1" , "facility_id" : "1"})
-//     expect(body).toEqual({
-//       "_id" : 1,
-//       "facility" : {
-//               "facility_status" : "normal",
-//               "facilityType" : "studys",
-//               "facilityTitle" : "The Irving K. Barber Learning Centre",
-//               "facilityDescription" : "The Irving K. Barber Learning Centre is a facility at the Vancouver campus of the University of British Columbia. The Learning Centre is built around the refurbished core of the 1925 UBC Main Library.",
-//               "timeAdded" : "2022/07/06",
-//               "facilityImageLink" : "https://cdn.discordapp.com/attachments/984213736652935230/994316357028036738/unknown.png",
-//               "facilityOverallRate" : 2.5,
-//               "numberOfRates" : 1,
-//               "longitude" : -123.2527,
-//               "latitude" : 49.2675
-//       },
-//       "ratedUser" : [
-//               {
-//                       "replierID" : "l2542293790@gmail.com"
-//               }
-//       ],
-//       "reviews" : [
-//               {
-//                       "replierID" : "l2542293790@gmail.com",
-//                       "userName" : "Linxin Li",
-//                       "rateScore" : 2.5,
-//                       "upVotes" : 0,
-//                       "downVotes" : 0,
-//                       "replyContent" : "testing ",
-//                       "timeOfReply" : "2022/6/18/23/41/5"
-//               }
-//       ],
-//       "adderID" : ""
-// })    
-//   expect(body.statusCode).toEqual(200)
-//    });
-
-// });
+ 
