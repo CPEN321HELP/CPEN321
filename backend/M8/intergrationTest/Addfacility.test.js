d920cadb7bdc6515ad0c396d1492605cb889197d
PresidentXijiping <88121278+PresidentXijiping@users.noreply.github.com>
2022-08-05 22:34:07 -0700

    Update Addfacility.test.js

--- backend/M8/intergrationTest/Addfacility.test.js
+++ backend/M8/intergrationTest/Addfacility.test.js
@@ -1,4 +1,5 @@
 const express = require("express"); // import express
+
 const app = express(); //an instance of an express app, a 'fake' express app
 const request = require('supertest');
 const server  = require('../server6.js');
